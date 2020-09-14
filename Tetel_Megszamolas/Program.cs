bfc0ff7a65eceeed87b1825f3dbd5072e22d6dbf
borossandor27 <boros.sanyi@gmail.com>
2020-09-14 18:42:25 +0200

    Add project files.

---
+++ Tetel_Megszamolas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetel_Megszamolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-- Bekéri az elemek számát
+            Console.WriteLine("Kérem az elemek számát!");
+            int N;
+            while (!(int.TryParse(Console.ReadLine(),out N) && N>0))
+            {
+                Console.WriteLine("Nem megfelelő érték! Adjon újat!");
+            }
+            //-- bekéri az elemeket
+            int[] T = new int[N];
+            for (int i = 0; i < T.Length; i++)
+            {
+                Console.Write($"kérem a {i+1} elemet: ");
+                int tmp;
+                while (!(int.TryParse(Console.ReadLine(), out tmp)))
+                {
+                    Console.WriteLine($"Nem megfelelő érték! Adjon új {i+1}-k elemet!");
+                }
+                T[i] = tmp;
+            }
+
+            Console.WriteLine($"Az elemek: {String.Join(", ", T)}");
+            Console.WriteLine("Program vége!");
+            Console.ReadKey();
+        }
+    }
+}
